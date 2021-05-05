ac7785daa0a6a64e0ddcdf0f753cf9c2c9e62f98
Welder Askowiks <askowiks@gmail.com>
2021-05-05 17:02:50 -0300

    Desafio3

---
+++ primeiroCodigoJava/Desafio3.java
@@ -0,0 +1,18 @@
+public class Desafio3 {
+    public static void main (String[] args){
+        double Computador = 3590.00;
+        double Mouse = 45.60;
+        double Teclado = 123.80;
+        double Total = Computador + Mouse + Teclado;
+
+        System.out.println(
+        "       Resumo da compra:" +
+        "\n Computador   R$ " + Computador +
+        "\n Mouse        R$   " + Mouse + 
+        "\n Teclado      R$  " + Teclado +
+        "\n Total        R$ " + Total +
+        "\n Obrigado pela preferencia!"
+         );
+    }
+}
+    
