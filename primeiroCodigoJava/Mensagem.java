14fa08a8531719a91af15b153082762cef8b8690
Welder Askowiks <askowiks@gmail.com>
2021-05-05 14:36:10 -0300

    Criei meu primeiro codigo java

---
+++ primeiroCodigoJava/Mensagem.java
@@ -0,0 +1,7 @@
+class Mensagem {
+    public static void main(String[] args){
+        //System.out.println("Olá!");
+        System.out.println("Olá " + args [0] + " " + args [1] + " seu lindo!");
+        System.out.println ("Volte sempre!");
+    }
+}
